5e5f030b8b43bf55d7f28bcb2dc457a720bb8c83
Ahmad Karimi <ak12hastam@gmail.com>
2023-06-14 20:59:39 +0330

    watch owned secrets

--- internal/controller/route/controller.go
+++ internal/controller/route/controller.go
@@ -3,6 +3,7 @@ package route
 import (
 	routev1 "github.com/openshift/api/route/v1"
 	contourv1 "github.com/projectcontour/contour/apis/projectcontour/v1"
+	v1 "k8s.io/api/core/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
 
@@ -10,5 +11,6 @@ func (r *RouteReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&routev1.Route{}).
 		Owns(&contourv1.HTTPProxy{}).
+		Owns(&v1.Secret{}).
 		Complete(r)
 }
